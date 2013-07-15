30271ec3ae1e3be427585777d505b031f6411309
sdhery@gmail.com <herys@163.com>
2013-07-15 17:56:13 +0800

    提交代码

--- src/main/java/com/sdhery/module/info/dao/IInfoArticleDao.java
+++ src/main/java/com/sdhery/module/info/dao/IInfoArticleDao.java
@@ -1,7 +1,10 @@
 package com.sdhery.module.info.dao;
 
+import com.sdhery.module.core.commons.Condition;
 import com.sdhery.module.core.dao.EntityDao;
 import com.sdhery.module.info.domain.InfoArticle;
+
+import java.util.List;
 
 /**
 * @Title:数据库操作类接口
@@ -10,4 +13,5 @@ import com.sdhery.module.info.domain.InfoArticle;
 * @Date:2013-07-09 10:41:16
 */
 public interface IInfoArticleDao extends EntityDao<InfoArticle,Integer> {
+    List<InfoArticle> search(Condition condition);
 }
--- src/main/java/com/sdhery/module/info/dao/impl/InfoArticleDao.java
+++ src/main/java/com/sdhery/module/info/dao/impl/InfoArticleDao.java
@@ -1,8 +1,11 @@
 package com.sdhery.module.info.dao.impl;
 
+import com.sdhery.module.core.commons.Condition;
 import com.sdhery.module.core.dao.impl.BaseMybatisDao;
 import com.sdhery.module.info.dao.IInfoArticleDao;
 import com.sdhery.module.info.domain.InfoArticle;
+
+import java.util.List;
 
 /**
 * @Title:数据库操作实现类
@@ -11,4 +14,7 @@ import com.sdhery.module.info.domain.InfoArticle;
 * @Date:2013-07-09 10:41:16
 */
 public class InfoArticleDao extends BaseMybatisDao<InfoArticle, Integer> implements IInfoArticleDao{
+    public List<InfoArticle> search(Condition condition) {
+        return getSqlSession().selectList(getClass().getName() + ".search",condition);
+    }
 }
